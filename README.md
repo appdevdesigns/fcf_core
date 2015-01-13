4ed8abe86dccedb674b5311cdb034bc0d0ac51e3
Johnny Hausman <johnny@appdevdesigns.net>
2015-01-13 17:45:28 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+fcf_core ![Build Status](https://travis-ci.org/appdevdesigns/fcf_core.png)
+================
+
+A new plugin.
---
+++ config/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Routes
+ *
+ * Use this file to add any module specific routes to the main Sails
+ * route object.
+ */
+
+
+module.exports = {
+
+
+  /*
+
+  '/': {
+    view: 'user/signup'
+  },
+  '/': 'fcf_core/PluginController.inbox',
+  '/': {
+    controller: 'fcf_core/PluginController',
+    action: 'inbox'
+  },
+  'post /signup': 'fcf_core/PluginController.signup',
+  'get /*(^.*)' : 'fcf_core/PluginController.profile'
+
+  */
+
+
+};
+
